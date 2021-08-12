f234b39d48c5e7902fa0fc3e515c0f87acd14433
Carlos Caceres <carloscaceres041@gmail.com>
2021-08-12 03:44:11 -0700

    stage1 done

--- para/paraAPP/views.py
+++ para/paraAPP/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    return HttpResponse("routed")
+    return render(request, "index.html")
+
+def about_us(request):
+    return render(request, "about_us.html")
+
+def press(request):
+    return render(request, "press.html")
